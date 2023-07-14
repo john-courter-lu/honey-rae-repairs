8fadb010fc937b8777862c1a4b52b9c2deacc7f5
John Lu <johnlu2006@gmail.com>
2023-07-14 11:46:39 -0500

    chap 13 done. chap 12 skipped

--- src/components/profile/EmployeeForm.js
+++ src/components/profile/EmployeeForm.js
@@ -11,7 +11,7 @@ export const EmployeeForm = () => {
     const localHoneyUser = localStorage.getItem("honey_user")
     //  grabs honey_user out of storage
     // ^string, needs to be converted into an objext
-    const honeyUserObject = JSON.parse(localHoneyUser) 
+    const honeyUserObject = JSON.parse(localHoneyUser)
     // devtools -> application -> storage -> localhost -> now shows the value of staff and id under it 
     //stole from ticketList.js/////////////////
 
@@ -32,80 +32,83 @@ export const EmployeeForm = () => {
             TODO: Perform the PUT fetch() call here to update the profile.
             Navigate user to home page when done.
         */
-       return fetch(`http://localhost:8088/employees/${profile.id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(profile)
+        return fetch(`http://localhost:8088/employees/${profile.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(profile)
 
-       })
+        })
             .then(response => response.json())
             .then(() => {
                 setFeedback("Employee profile successfully saved")
-            })
-        }
-            const [feedback, setFeedback] = useState("")
+            }) //这是为显示消息框 章13 节2
+    }
 
-            useEffect(() => {
-                if (feedback !== "") {
-                    // Clear feedback to make entire element disappear after 3 seconds
-                    setTimeout(() => setFeedback(""), 3000);
-                }
-            }, [feedback])
-    
-     //// ^ devtools -> network -> clear network -> user enters new state -> 1 request URL to employee.id -. status code 200 ok means it works 
+    const [feedback, setFeedback] = useState("") //这是为显示消息框 章13 节2
+
+    useEffect(() => {
+        if (feedback !== "") {
+            // Clear feedback to make entire element disappear after 3 seconds
+            setTimeout(() => setFeedback(""), 3000);
+        }
+    }, [feedback]) //这是为消息框的动态效果 章13 节2. 还需要return中的<div>和index.css
+
+    //// ^ devtools -> network -> clear network -> user enters new state -> 1 request URL to employee.id -. status code 200 ok means it works 
     return (
         <>
-        <form className="profile">
-            <h2 className="profile__title">New Service Ticket</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="specialty">Specialty:</label>
-                    <input
-                        required autoFocus
-                        type="text"
-                        className="form-control"
-                        value={profile.specialty}
-                        onChange={
-                            (evt) => {
-                                // TODO: Update specialty property
-                                const copy = {...profile}
-                                copy.specialty = evt.target.value
-                                updateProfile(copy)
-                            }
-                        } />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="name">Hourly rate:</label>
-                    <input type="number"
-                        className="form-control"
-                        value={profile.rate}
-                        onChange={
-                            (evt) => {
-                                // TODO: Update rate property
-                                // even tho the type is number it will always return a string
-                                // unlessss you wrap it in a parse 
-                                const copy = {...profile}
-                                copy.rate = parseFloat(evt.target.value, 2) //float is for decimal, 2 is for        components -> applicationviews -> employeeviews -> profile -> employee form -> changes state
-                                //got initial state fethed from the api for the permanent state and updated the  component state and now capturing what the user did .next step saving
-                                updateProfile(copy)
-                            }
-                        } />
-                </div>
-            </fieldset>
-            <button
-                onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
-                className="btn btn-primary">
-                Save Profile
-            </button>
-        </form>
-        <div className={`${feedback.includes("Error") ? "error" : "feedback"} ${feedback === "" ? "invisible" : "visible"}`}>
-    {feedback}
-</div>
-      </>  
+            <div className={`${feedback.includes("Error") ? "error" : "feedback"} ${feedback === "" ? "invisible" : "visible"}`}>
+                {feedback}
+            </div>
+            
+            <form className="profile">
+                <h2 className="profile__title">New Service Ticket</h2>
+                <fieldset>
+                    <div className="form-group">
+                        <label htmlFor="specialty">Specialty:</label>
+                        <input
+                            required autoFocus
+                            type="text"
+                            className="form-control"
+                            value={profile.specialty}
+                            onChange={
+                                (evt) => {
+                                    // TODO: Update specialty property
+                                    const copy = { ...profile }
+                                    copy.specialty = evt.target.value
+                                    updateProfile(copy)
+                                }
+                            } />
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div className="form-group">
+                        <label htmlFor="name">Hourly rate:</label>
+                        <input type="number"
+                            className="form-control"
+                            value={profile.rate}
+                            onChange={
+                                (evt) => {
+                                    // TODO: Update rate property
+                                    // even tho the type is number it will always return a string
+                                    // unlessss you wrap it in a parse 
+                                    const copy = { ...profile }
+                                    copy.rate = parseFloat(evt.target.value, 2) //float is for decimal, 2 is for        components -> applicationviews -> employeeviews -> profile -> employee form -> changes state
+                                    //got initial state fethed from the api for the permanent state and updated the  component state and now capturing what the user did .next step saving
+                                    updateProfile(copy)
+                                }
+                            } />
+                    </div>
+                </fieldset>
+                <button
+                    onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+                    className="btn btn-primary">
+                    Save Profile
+                </button>
+            </form>
+
+        </>
     )
 }
 //// need <></> to add multiple things in return div & form
